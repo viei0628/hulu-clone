4c7e59ad2cb924dbbd4207056a886ccf5746d44f
Timothy Vieira <vieira.timothy@yahoo.ca>
2021-08-11 08:27:42 -0400

    Added more styling

--- src/Nav.js
+++ src/Nav.js
@@ -5,35 +5,31 @@ import requests from "./requests";
 function Nav({ setSelectedOption }) {
   return (
     <div className="nav">
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchTrending)}>
+      <h2 onClick={() => setSelectedOption(requests.fetchTrending)}>
         Trending
       </h2>
       {/* <h2 onClick={() => setSelectedOption(requests.fetchTopRated)}>
         Top Rated
       </h2> */}
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchActionMovies)}>
+      <h2 onClick={() => setSelectedOption(requests.fetchActionMovies)}>
         Action
       </h2>
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchComedyMovies)}>
+      <h2 onClick={() => setSelectedOption(requests.fetchComedyMovies)}>
         Comedy
       </h2>
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchHorrorMovies)}>
+      <h2 onClick={() => setSelectedOption(requests.fetchHorrorMovies)}>
         Horror
       </h2>
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchRomanceMovies)}>
+      <h2 onClick={() => setSelectedOption(requests.fetchRomanceMovies)}>
         Romance
       </h2>
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchSciFi)}>Sci-fi</h2>
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchMystery)}>
-        Mystery
-      </h2>
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchWestern)}>
-        Western
-      </h2>
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchAnimation)}>
+      <h2 onClick={() => setSelectedOption(requests.fetchSciFi)}>Sci-fi</h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchMystery)}>Mystery</h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchWestern)}>Western</h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchAnimation)}>
         Animation
       </h2>
-      <h2 onMouseOver={() => setSelectedOption(requests.fetchTV)}>TV</h2>
+      <h2 onClick={() => setSelectedOption(requests.fetchTV)}>TV</h2>
     </div>
   );
 }
